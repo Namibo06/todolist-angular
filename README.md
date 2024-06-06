7dfbb0c04805b1ff9e43646ddaa5101d5cf69c6c
Namibo06 <namibo654@gmail.com>
2024-06-05 23:52:23 -0300

    style:index

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-//register
-//login
+//register V
+//login V
 //index
 //perfil
 //update-user
 //delete-user
-//task/{id}
-//create-task
-//update-task
+//task/{id} V
+//create-task V
+//update-task V
 //delete-task
+
+//ver questão de por background como rgba(0,0,0,0.2) em toda a tela,enquanto na index estiver aberto o botão
--- src/app/pages/index/index/index.component.ts
+++ src/app/pages/index/index/index.component.ts
@@ -30,6 +30,15 @@ export class IndexComponent implements OnInit{
     this.findAllTask();
   }
 
+  onAnchorClick(event: Event) {
+    event.preventDefault(); // Evita o comportamento padrão de redirecionamento do link
+  }
+
+  onButtonClick(event: Event) {
+    event.stopPropagation(); // Impede a propagação do evento de clique para o elemento pai (a âncora)
+    // Coloque aqui o código que deseja executar quando o botão é clicado
+  }
+
   findAllTask(){
     this.service.findAllTaskService(this.user_id).subscribe({
       next:(res)=>{
